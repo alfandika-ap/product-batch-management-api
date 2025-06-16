857f4037980e7356a8cc379610fe37b20ac4c6b0
alfandika-ap <dikalfan@gmail.com>
2025-06-16 23:52:45 +0700

    fix env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,21 +4,19 @@ module.exports = {
     script: 'bun',
     args: 'src/index.ts',
     interpreter: '',
-    env: {
-      NODE_ENV: 'development',
-      PORT: 3000
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
     watch: false,
     instances: 1,
     exec_mode: 'fork',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
     max_memory_restart: '1G',
     error_file: 'logs/err.log',
     out_file: 'logs/out.log',
     merge_logs: true,
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
   }]
 }; 
