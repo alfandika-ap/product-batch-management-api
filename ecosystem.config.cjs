3655c2b1721ac4a34b313216baa537a22c5116b0
alfandika-ap <dikalfan@gmail.com>
2025-06-16 22:49:47 +0700

    fix: deployment

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'carabao-product-management-api',
+    script: 'bun',
+    args: 'src/index.ts',
+    interpreter: '',
+    env: {
+      NODE_ENV: 'production',
+    },
+    watch: false,
+    instances: 1,
+    exec_mode: 'fork',
+    max_memory_restart: '1G',
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+  }]
+}; 
